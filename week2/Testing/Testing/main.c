66c502df9f954ae27a024d540728c971fbdc09ab
aiemani <nabroman42@icloud.com>
2020-08-25 13:18:14 +0300

    Testing Xcode with git

---
+++ week2/Testing/Testing/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  Testing
+//
+//  Created by Roman Nabiullin on 25.08.2020.
+//  Copyright © 2020 Innopolis Univesity. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
