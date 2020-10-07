46bdd0ac5dafdc2e3c9ed2d2c6094c2acd8b5b7b
aiemani <nabroman42@icloud.com>
2020-10-07 15:35:17 +0000

    Lab 8 is done

---
+++ week8/ex4.c
@@ -0,0 +1,23 @@
+#include <unistd.h>  // for sleep()
+#include <stdlib.h>  // for malloc()
+#include <string.h>  // for memset()
+#include <stdio.h>
+#include <sys/resource.h> 
+
+int main() {
+  int size = 10 * 1024 * 1024;    // 10 MB size
+  struct rusage usage;
+  int RUSAGE_SELF;
+
+  for (int i = 0; i < 10; i++) {
+    void* ptr = malloc(size);             // allocating 10 MB memory
+    memset(ptr, 0, size);                 // filling it with '0'
+
+    getrusage(RUSAGE_SELF, &usage);       // retrieving information about memory usage
+		printf("%ld KB\n", usage.ru_maxrss);  // printing 
+
+    sleep(1);                             // sleep for 1 sec at each iteration
+  }
+
+  return 0;
+}
